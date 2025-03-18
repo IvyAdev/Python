599501967e53c1f5b4ccf43417dd03ca39c1cc16
IvyAdev <anyangoivy1@gmail.com>
2025-03-18 11:07:34 -0700

    Initial commit

---
+++ greeting.py
@@ -0,0 +1,7 @@
+def greeting_app():
+    name = input("What's your name? ")
+    color = input("What's your favorite color? ")
+    
+    print(f"Hello, {name}! Your favorite color, {color}, is awesome!")
+    
+greeting_app()
