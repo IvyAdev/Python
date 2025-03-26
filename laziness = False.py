1fab06a65d1e016bb5c68517a7b8452e2fdb0d10
IvyAdev <anyangoivy1@gmail.com>
2025-03-26 13:06:08 -0700

    Initial commit of Week 2 assignment

---
+++ laziness = False.py
@@ -0,0 +1,9 @@
+laziness = False
+hunger = True
+
+if laziness > hunger:
+    print("You are lazy")
+elif hunger > laziness:
+    print("You are hungry")
+else:
+    print("You are neither hungry nor lazy")
