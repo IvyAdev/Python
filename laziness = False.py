9fa87954d033f0b6b487f394603cd3d7412926ab
IvyAdev <anyangoivy1@gmail.com>
2025-03-26 13:55:46 -0700

    Delete laziness = False.py

--- laziness = False.py
+++
@@ -1,9 +0,0 @@
-laziness = False
-hunger = True
-
-if laziness > hunger:
-    print("You are lazy")
-elif hunger > laziness:
-    print("You are hungry")
-else:
-    print("You are neither hungry nor lazy")
